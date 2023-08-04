332f5d8e35b5e0d8a0a8042fa18448d06f7992c6
Jorgean Bomfim <bonfimjorgean@gmail.com>
2023-08-04 13:40:15 -0300

    aplicando bordas nas imagens

--- app.py
+++ app.py
@@ -8,11 +8,9 @@ st.set_page_config(
 )
 
 def create_whatsapp_link(phone_number, message):
-    base_url = f"https://wa.me/{phone_number}/"
-    if message:
-        return f"{base_url}?text={message}"
-    else:
-        return base_url
+    base_url = "https://api.whatsapp.com/send/"
+    params = f"?phone={phone_number}&text={message}&type=phone_number&app_absent=0"
+    return base_url + params
 
 def show_products(products, title, description):
     st.subheader(title)
@@ -21,6 +19,9 @@ def show_products(products, title, description):
     num_products = len(products)
     num_columns = 4  # Exibir 4 produtos em uma fileira
 
+    # Define o tamanho das colunas para ajustar o espaçamento
+    col_width = 1 / num_columns
+
     for i in range(0, num_products, num_columns):
         product_row = products[i:i + num_columns]
 
@@ -28,37 +29,39 @@ def show_products(products, title, description):
 
         for idx, product in enumerate(product_row):
             with cols[idx]:
-                st.image(product['image_url'], use_column_width=True, caption=product['name'], output_format='auto')
-                st.write(product['description'])
+                # Utiliza um wrapper de div para aplicar estilos de borda
+                st.markdown(
+                    f'<div style="border: 2.0px solid #eaeaea; padding: 3px;">'
+                    f'<img src="{product["image_url"]}" alt="{product["name"]}" style="width: 100%;"/>'
+                    f'<p><strong>{product["name"]}</strong></p>'
+                    f'<p>Descrição: {product["description"]}</p>'
+                    f'<p>Valor: {product["value"]}</p>'
+                    '</div>',
+                    unsafe_allow_html=True
+                )
 
 def main():
     st.title("App do MRF")
 
     products_col_1 = [
-        {"name": "Produto 1", "description": "Descrição do Produto 1", "image_url": "https://cdn.leroymerlin.com.br/contents/40_tipos_de_ferramentas_nomes_e_para_que_servem_345c_original.jpg"},
-        {"name": "Produto 2", "description": "Descrição do Produto 2", "image_url": "https://cdn.leroymerlin.com.br/contents/40_tipos_de_ferramentas_nomes_e_para_que_servem_c3ee_original.jpg"},
-        {"name": "Produto 3", "description": "Descrição do Produto 3", "image_url": "https://cdn.leroymerlin.com.br/contents/40_tipos_de_ferramentas_nomes_e_para_que_servem_2b12_original.jpg"},
-        {"name": "Produto 4", "description": "Descrição do Produto 4", "image_url": "https://cdn.leroymerlin.com.br/contents/40_tipos_de_ferramentas_nomes_e_para_que_servem_554c_original.jpg"}
+        {"name": "Produto 1", "value": "50,99R$", "description": "Descrição do Produto 1...", "image_url": "https://cdn.leroymerlin.com.br/contents/40_tipos_de_ferramentas_nomes_e_para_que_servem_345c_original.jpg"},
+        {"name": "Produto 2", "value": "50,99R$", "description": "Descrição do Produto 2...", "image_url": "https://cdn.leroymerlin.com.br/contents/40_tipos_de_ferramentas_nomes_e_para_que_servem_c3ee_original.jpg"},
+        {"name": "Produto 3", "value": "50,99R$", "description": "Descrição do Produto 3...", "image_url": "https://cdn.leroymerlin.com.br/contents/40_tipos_de_ferramentas_nomes_e_para_que_servem_2b12_original.jpg"},
+        {"name": "Produto 4", "value": "50,99R$", "description": "Descrição do Produto 4...", "image_url": "https://cdn.leroymerlin.com.br/contents/40_tipos_de_ferramentas_nomes_e_para_que_servem_554c_original.jpg"}
     ]
 
 
     products_col_2 = [
-        {"name": "Produto 1", "description": "Descrição do Produto 1", "image_url": "https://cdn.leroymerlin.com.br/products/chave_de_cano_grifo_heavy_duty_36_91cm_lotus_ref_5281_1567036098_ffcf_600x600.jpg"},
-        {"name": "Produto 2", "description": "Descrição do Produto 2", "image_url": "https://down-br.img.susercontent.com/file/0a9b3ac3f254598a19dd86a7ee9f58a4"},
-        {"name": "Produto 3", "description": "Descrição do Produto 3", "image_url": "https://chatuba.vtexassets.com/arquivos/ids/160824-800-auto?v=637147144152500000&width=800&height=auto&aspect=true"},
-        {"name": "Produto 4", "description": "Descrição do Produto 4", "image_url": "https://cdn.awsli.com.br/1659/1659184/produto/93887665/b56e1e85f3.jpg"}
+        {"name": "Produto 1", "value": "50,99R$", "description": "Descrição do Produto 1...", "image_url": "https://cdn.leroymerlin.com.br/products/chave_de_cano_grifo_heavy_duty_36_91cm_lotus_ref_5281_1567036098_ffcf_600x600.jpg"},
+        {"name": "Produto 2", "value": "50,99R$", "description": "Descrição do Produto 2...", "image_url": "https://down-br.img.susercontent.com/file/0a9b3ac3f254598a19dd86a7ee9f58a4"},
+        {"name": "Produto 3", "value": "50,99R$", "description": "Descrição do Produto 3...", "image_url": "https://chatuba.vtexassets.com/arquivos/ids/160824-800-auto?v=637147144152500000&width=800&height=auto&aspect=true"},
+        {"name": "Produto 4", "value": "50,99R$", "description": "Descrição do Produto 4...", "image_url": "https://cdn.awsli.com.br/1659/1659184/produto/93887665/b56e1e85f3.jpg"}
     ]
     st.header("Produtos em Estoque")
 
     show_products(products_col_1, "Ferramentas", "Temos uma grande variedade de ferramentas disponíveis.")
     show_products(products_col_2, "Encanamento", "Tudo que você precisa para trabalhos de encanamento.")
 
-
-
-    def create_whatsapp_link(phone_number, message):
-        base_url = "https://api.whatsapp.com/send/"
-        params = f"?phone={phone_number}&text={message}&type=phone_number&app_absent=0"
-        return base_url + params
 
     phone_number = "5521979215685"
     message = "Olá, estou interessado em comprar o produto XYZ."
@@ -69,7 +72,7 @@ def main():
     with open(r"WhatsApp-icone.png", "rb") as img_file:
         whatsapp_icon = base64.b64encode(img_file.read()).decode()
 
-    button_html = f"""
+    button_whatsapp = f"""
         <a href="{whatsapp_link}" target="_blank">
             <button style="background-color: #25d366; border: none; color: #ffffff; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 4px; cursor: pointer;">
                 <img src="data:image/png;base64,{whatsapp_icon}" alt="WhatsApp Icon" width="20" style="margin-right: 10px;">
@@ -77,8 +80,9 @@ def main():
             </button>
         </a>
     """
-    st.markdown(button_html, unsafe_allow_html=True)
+    '\n'
+    '\n'
+    st.markdown(button_whatsapp, unsafe_allow_html=True)
 
-        
 if __name__ == "__main__":
     main()
